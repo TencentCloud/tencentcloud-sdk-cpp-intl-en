08be2bdc55abe14553baaf430ee0559b9f4a0907
tencentcloudapi <tencentcloudapi@tencent.com>
2024-09-27 04:57:34 +0800

    release 3.0.664

--- ocr/include/tencentcloud/ocr/v20181119/model/MLIDPassportOCRResponse.h
+++ ocr/include/tencentcloud/ocr/v20181119/model/MLIDPassportOCRResponse.h
@@ -294,6 +294,36 @@ Note: This field may return null, indicating that no valid values can be obtaine
                      */
                     bool PassportRecognizeInfosHasBeenSet() const;
 
+                    /**
+                     * 获取Card Warning Information
+
+-9101 Alarm for covered certificate,
+-9102 Alarm for photocopied certificate,
+-9103 Alarm for photographed certificate,
+-9104 Alarm for PS certificate,
+-9107 Alarm for reflective certificate,
+-9108 Alarm for blurry image,
+-9109 This capability is not enabled.
+                     * @return WarnCardInfos Card Warning Information
+
+-9101 Alarm for covered certificate,
+-9102 Alarm for photocopied certificate,
+-9103 Alarm for photographed certificate,
+-9104 Alarm for PS certificate,
+-9107 Alarm for reflective certificate,
+-9108 Alarm for blurry image,
+-9109 This capability is not enabled.
+                     * 
+                     */
+                    std::vector<int64_t> GetWarnCardInfos() const;
+
+                    /**
+                     * 判断参数 WarnCardInfos 是否已赋值
+                     * @return WarnCardInfos 是否已赋值
+                     * 
+                     */
+                    bool WarnCardInfosHasBeenSet() const;
+
                 private:
 
                     /**
@@ -404,6 +434,20 @@ Note: This field may return null, indicating that no valid values can be obtaine
                      */
                     PassportRecognizeInfos m_passportRecognizeInfos;
                     bool m_passportRecognizeInfosHasBeenSet;
+
+                    /**
+                     * Card Warning Information
+
+-9101 Alarm for covered certificate,
+-9102 Alarm for photocopied certificate,
+-9103 Alarm for photographed certificate,
+-9104 Alarm for PS certificate,
+-9107 Alarm for reflective certificate,
+-9108 Alarm for blurry image,
+-9109 This capability is not enabled.
+                     */
+                    std::vector<int64_t> m_warnCardInfos;
+                    bool m_warnCardInfosHasBeenSet;
 
                 };
             }
